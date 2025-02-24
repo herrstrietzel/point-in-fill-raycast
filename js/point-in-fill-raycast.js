700db3bfa970cb9b25300cb642cadf433162db98
herrstrietzel <36772754+herrstrietzel@users.noreply.github.com>
2025-02-24 04:11:59 +0100

    Update point-in-fill-raycast.js
    added point in stroke

--- js/point-in-fill-raycast.js
+++ js/point-in-fill-raycast.js
@@ -1,4 +1,8 @@
-function isPointInPathData(d, pt, precision = 10) {
+/**
+ * Check point in path
+ * only suited for single point checks
+ */
+function isPointInPathData(d, pt, precision = 24, evenOdd=false) {
 
     let pathData, compoundPoly;
 
@@ -6,7 +10,7 @@ function isPointInPathData(d, pt, precision = 10) {
     let hasCurvesOrShorthands = (/[C|Q|T|S|H|V]/gi).test(d);
     let hasRelativeLinetos = (/[l]/g).test(d);
     let hasRelativeMovetos = (/[m]/g).test(d);
-    let isPoly = !hasCurvesOrShorthands && !hasRelativeLinetos && !hasRelativeMovetos
+    let isPoly = !hasCurvesOrShorthands && !hasRelativeLinetos && !hasRelativeMovetos;
 
 
     if (!isPoly) {
@@ -17,8 +21,7 @@ function isPointInPathData(d, pt, precision = 10) {
     //path data contains only linetos
     else {
         let poly = d.replace(/[m|l|z]/gi, ' ').split(/,| /).filter(Boolean)
-        compoundPoly = [{ vertices: [], bb: {} }]
-        //let pol = {vertices:[], bb:{}}
+        compoundPoly = [{ vertices: [], bb: {}, lowPoly:[] }]
         for (let i = 1; i < poly.length; i += 2) {
             compoundPoly[0].vertices.push({ x: +poly[i - 1], y: +poly[i] })
         }
@@ -26,27 +29,115 @@ function isPointInPathData(d, pt, precision = 10) {
         compoundPoly[0].bb = getPolyBBox(compoundPoly[0].vertices);
     }
 
-    let pointsInPoly = 0;
-    for (let i = 0; i < compoundPoly.length; i++) {
-        let { vertices, bb } = compoundPoly[i];
-        if (isPointInPolygon(pt, vertices, bb, false)) {
-            pointsInPoly++;
-        }
-    }
-    return pointsInPoly%2 !== 0;
+    let pointInPoly = isPointInFillCompoundPolygon(compoundPoly, pt, evenOdd)
+    return pointInPoly;
+
 }
 
 
-function isPointInCompoundPolygon(compoundPoly, pt) {
+
+function isPointInFillCompoundPolygon(compoundPoly, pt, evenOdd = false) {
     let pointsInPoly = 0;
     for (let i = 0; i < compoundPoly.length; i++) {
-        let { vertices, bb } = compoundPoly[i];
-        if (isPointInPolygon(pt, vertices, bb)) {
+        let { vertices, bb, lowPoly } = compoundPoly[i];
+
+        // not in bbox - skip
+        if (bb.left > pt.x || bb.top > pt.y || bb.bottom < pt.y || bb.right < pt.x) {
+            continue;
+        }
+
+        //try lowPoly first for higher precision settings
+        if (lowPoly.length && isPointInFillPolygon(pt, lowPoly, bb, evenOdd)) {
             pointsInPoly++;
+        } else {
+            if (isPointInFillPolygon(pt, vertices, bb, evenOdd)) {
+                pointsInPoly++;
+            }
+        }
+
+    }
+
+    return pointsInPoly % 2 !== 0;
+}
+
+
+function isPointInStrokeCompoundPolygon(compoundPoly, pt, strokeWidth = 0) {
+    let pointsInStroke = false;
+
+    for (let i = 0; i < compoundPoly.length; i++) {
+        let { vertices, bb } = compoundPoly[i];
+
+        let strokeHalf = strokeWidth / 2
+
+        // not in bbox - skip
+        if (bb.left - strokeHalf > pt.x || bb.top - strokeHalf > pt.y || bb.bottom + strokeHalf < pt.y || bb.right + strokeHalf < pt.x) {
+            continue;
+        }
+
+        pointsInStroke = isPointInStrokePoly(pt, vertices, strokeWidth)
+        if (pointsInStroke) return true
+
+    }
+
+    return pointsInStroke;
+}
+
+
+/**
+ * emulate pointInStroke
+ */
+
+function isPointInStrokePoly(pt, polygon, strokeWidth, tolerance = 1) {
+
+    const getProjectedPoint = (pt, A, B) => {
+
+        let dx = B.x - A.x;
+        let dy = B.y - A.y;
+        let lengthSq = dx * dx + dy * dy;
+
+        // Project the point onto the line segment
+        let t = ((pt.x - A.x) * dx + (pt.y - A.y) * dy) / lengthSq;
+        let tClamped = Math.max(0, Math.min(1, t)); // Clamp to the segment
+        return {
+            x: A.x + tClamped * dx,
+            y: A.y + tClamped * dy
+        };
+
+    }
+
+
+    let halfStrokeSq = (strokeWidth / 2) ** 2;
+
+    for (let i = 0; i < polygon.length; i++) {
+        let A = polygon[i];
+        let B = polygon[(i + 1) % polygon.length];
+
+        let projected = getProjectedPoint(pt, A, B);
+        let diffX = Math.abs(projected.x - pt.x);
+        let diffY = Math.abs(projected.y - pt.y);
+        let diff = (diffX + diffY) / 2
+
+        // average distance too large
+        if (diff > strokeWidth / 2) {
+            continue
+        }
+
+        // squared distance from the point to the edge
+        let distanceSq = (pt.x - projected.x) ** 2 + (pt.y - projected.y) ** 2;
+
+
+        // distance within the squared stroke width or diff < 1
+        if (distanceSq <= halfStrokeSq || diff < tolerance) {
+            return true;
         }
     }
-    return pointsInPoly%2 !== 0;
+
+    return false;
+
 }
+
+
+
 
 
 
@@ -54,40 +145,62 @@ function isPointInCompoundPolygon(compoundPoly, pt) {
 * based on timepp's answer
 * https://stackoverflow.com/questions/217578/how-can-i-determine-whether-a-2d-point-is-within-a-polygon#63436180
 */
-function isPointInPolygon(pt, polygon, bb, skipBB = false) {
+
+
+function isPointInFillPolygon(pt, polygon, bb, evenOdd = false) {
     const between = (p, a, b) => (p >= a && p <= b) || (p <= a && p >= b);
+    let windingNumber = 0;
     let inside = false;
+    let A, B;
 
     // not in bbox - quit || no bbox defined
-    if (!skipBB || !bb.bottom) {
-        if (bb.left > pt.x || bb.top > pt.y || bb.bottom < pt.y || bb.right < pt.x) {
-            return false;
-        }
+    if (bb.left && (bb.left > pt.x || bb.top > pt.y || bb.bottom < pt.y || bb.right < pt.x)) {
+        return false;
     }
 
-    for (let i = polygon.length - 1, j = 0; j < polygon.length; i = j, j++) {
-        const A = polygon[i];
-        const B = polygon[j];
-        // corner cases
-        if ((pt.x == A.x && pt.y == A.y) || (pt.x == B.x && pt.y == B.y))
-            return true;
+    for (let len = polygon.length, i = len - 1, j = 0; j < len; i = j, j++) {
+        A = polygon[i];
+        B = polygon[j];
+
+        // point coincides with vertice
+        if ((pt.x == A.x && pt.y == A.y) || (pt.x == B.x && pt.y == B.y)) return true;
+
         if (A.y == B.y && pt.y == A.y && between(pt.x, A.x, B.x)) return true;
+
         if (between(pt.y, A.y, B.y)) {
+
             // if pt inside the vertical range
             // filter out "ray pass vertex" problem by treating the line a little lower
             if ((pt.y == A.y && B.y >= A.y) || (pt.y == B.y && A.y >= B.y)) continue;
+
             // calc cross product `ptA X ptB`, pt lays on left side of AB if c > 0
             const c = (A.x - pt.x) * (B.y - pt.y) - (B.x - pt.x) * (A.y - pt.y);
-            if (c == 0) return true;
-            if (A.y < B.y == c > 0) inside = !inside;
+            if (c == 0) { return true }
+
+            // check windings for polys with self-intersecting segments
+            if (!evenOdd) {
+                if (A.y < B.y) {
+                    if (c > 0) windingNumber++;
+                } else {
+                    if (c < 0) windingNumber--;
+                }
+            } else {
+                if (A.y < B.y == c > 0) inside = !inside;
+            }
         }
     }
-    return inside ? true : false;
+    return !evenOdd ? windingNumber !== 0 : inside;
 }
 
 
+/**
+ * Convert pathdata to polygon array
+ * if path has subpaths
+ * add bounding box info and
+ * low poly version for higher precisions
+ */
+function pathDataToCompoundPoly(d, precision = 24) {
 
-function pathDataToCompoundPoly(d, precision = 10) {
     // parse pathdata if stringified
     let pathData = Array.isArray(d) ? d : parsePathDataNormalized(d, { arcsToCubic: true });
 
@@ -95,43 +208,73 @@ function pathDataToCompoundPoly(d, precision = 10) {
     let compoundPoly = [];
 
     // subpath polygon
-    let subs = pathDataToPolygon(pathData, precision);
+    let subPolys = pathDataToPolygon(pathData, precision);
+    let subPolysLow = precision > 24 ? pathDataToPolygon(pathData, 12) : [];
 
-    for (let i = 0; i < subs.length; i++) {
-        let vertices = subs[i];
+    for (let i = 0, len = subPolys.length; i < len; i++) {
+        let vertices = subPolys[i];
+        let lowPoly = subPolysLow.length ? subPolysLow[i] : []
+
         //getbounding box
         let bb = getPolyBBox(vertices);
-        compoundPoly.push({ vertices: vertices, bb: bb });
+        compoundPoly.push({ vertices: vertices, bb: bb, lowPoly: lowPoly });
     }
 
     return compoundPoly;
 }
 
 
+/**
+ * pathdata to polygon
+ * splits beziers
+ */
 
-function getPolyBBox(vertices) {
-    let xArr = vertices.map((pt) => {
-        return pt.x;
-    });
-    let yArr = vertices.map((pt) => {
-        return pt.y;
-    });
-    let bb = {
-        left: Math.min(...xArr),
-        right: Math.max(...xArr),
-        top: Math.min(...yArr),
-        bottom: Math.max(...yArr)
-    };
-    return bb;
-}
-
-
-
-function pathDataToPolygon(pathData, precision = 10) {
+function pathDataToPolygon(pathData, precision = 24) {
     let M = { x: pathData[0].values[0], y: pathData[0].values[1] }
     let polys = [[]]
 
-    pathData.forEach((com, i) => {
+
+    /**
+     * estimate  bounding box from final on-path points
+     * to adjust curve splitting
+     */
+    let p;
+    let xMin = Infinity
+    let yMin = Infinity
+    let xMax = 0
+    let yMax = 0
+
+
+    for (let i = 0, len = pathData.length; i < len; i++) {
+        let com = pathData[i];
+        let { type, values } = com;
+        let valsL = values.length ? values.slice(-2) : null
+        //console.log(values, valsL);
+
+        if (valsL) {
+            let [x, y] = valsL;
+            if (x < xMin) {
+                xMin = x
+            }
+            if (y < yMin) {
+                yMin = y
+            }
+            if (x > xMax) {
+                xMax = x
+            }
+
+            if (y > yMax) {
+                yMax = y
+            }
+        }
+    }
+
+    let bb = { x: xMin, y: yMin, width: xMax - xMin, height: yMax - yMin }
+    let bbMinSize = Math.min(bb.width, bb.height)
+
+
+    for (let i = 0, len = pathData.length; i < len; i++) {
+        let com = pathData[i];
 
         let { type, values } = com;
         let comPrev = i > 0 ? pathData[i - 1] : pathData[0]
@@ -164,19 +307,150 @@ function pathDataToPolygon(pathData, precision = 10) {
                 cp1 = { x: values[0], y: values[1] };
                 cp2 = { x: values[2], y: values[3] };
 
+
                 let pts = type === 'C' ? [p0, cp1, cp2, p] : [p0, cp1, p]
 
-                for (let i = 1; i < precision; i++) {
-                    let pt = pointAtT(pts, 1 / precision * i)
+                let xArr = pts.map(pt => { return pt.x });
+                let yArr = pts.map(pt => { return pt.y });
+                let xMin = Math.min(...xArr)
+                let xMax = Math.max(...xArr)
+                let yMin = Math.min(...yArr)
+                let yMax = Math.max(...yArr)
+
+
+                let w = xMax - xMin;
+                let h = yMax - yMin;
+                let dimA = (w + h) / 2
+                let rat = 1 / bbMinSize * dimA
+                let precisionAdaptive = Math.ceil(precision * rat) * 2
+
+
+                for (let i = 1; i < precisionAdaptive; i++) {
+                    let pt = pointAtT(pts, 1 / precisionAdaptive * i)
                     vertices.push(pt)
                 }
                 break;
         }
-    })
+
+
+        //let d = 'M' + vertices.map(pt => { return [pt.x, pt.y] }).flat().join(' ')
+        //console.log(d);
+
+
+    }
+
+
     return polys
 }
 
 
+/**
+ * calculate polygon
+ * bounding box
+ */
+function getPolyBBox(vertices) {
+    let xArr = vertices.map((pt) => {
+        return pt.x;
+    });
+    let yArr = vertices.map((pt) => {
+        return pt.y;
+    });
+    let bb = {
+        left: Math.min(...xArr),
+        right: Math.max(...xArr),
+        top: Math.min(...yArr),
+        bottom: Math.max(...yArr)
+    };
+    return bb;
+}
+
+
+
+/**
+ * detect self-intersecting segments 
+ * in polygon
+ */
+function polyHasSelfIntersections(polygon) {
+
+    const lineBBIntersection = (a1, a2, b1, b2) => {
+        // Get the bounding boxes of the two segments
+        let aMinX = Math.min(a1.x, a2.x);
+        let aMaxX = Math.max(a1.x, a2.x);
+        let aMinY = Math.min(a1.y, a2.y);
+        let aMaxY = Math.max(a1.y, a2.y);
+
+        let bMinX = Math.min(b1.x, b2.x);
+        let bMaxX = Math.max(b1.x, b2.x);
+        let bMinY = Math.min(b1.y, b2.y);
+        let bMaxY = Math.max(b1.y, b2.y);
+
+        // Check if the bounding boxes overlap
+        return aMaxX >= bMinX && aMinX <= bMaxX && aMaxY >= bMinY && aMinY <= bMaxY;
+    }
+
+    const isPointOnSegment = (A, B, P) => {
+        let cross = (B.x - A.x) * (P.y - A.y) - (B.y - A.y) * (P.x - A.x);
+        if (Math.abs(cross) > Number.EPSILON) return false; // Not collinear
+        let dot = (P.x - A.x) * (B.x - A.x) + (P.y - A.y) * (B.y - A.y);
+        if (dot < 0) return false; // Outside A
+        let lenSq = (B.x - A.x) * (B.x - A.x) + (B.y - A.y) * (B.y - A.y);
+        if (dot > lenSq) return false; // Outside B
+        return true;
+    }
+
+
+    const lineIntersection = (A, B, C, D) => {
+        let ccw = (a, b, c) => (c.y - a.y) * (b.x - a.x) - (b.y - a.y) * (c.x - a.x);
+        let ccwABC = ccw(A, B, C);
+        let ccwABD = ccw(A, B, D);
+        let ccwCDA = ccw(C, D, A);
+        let ccwCDB = ccw(C, D, B);
+
+        // segments are crossing
+        if ((ccwABC * ccwABD < 0) && (ccwCDA * ccwCDB < 0)) return true;
+
+        // endpoint lies on the other segment
+        if (ccwABC === 0 && isPointOnSegment(A, B, C)) return true;
+        if (ccwABD === 0 && isPointOnSegment(A, B, D)) return true;
+        if (ccwCDA === 0 && isPointOnSegment(C, D, A)) return true;
+        if (ccwCDB === 0 && isPointOnSegment(C, D, B)) return true;
+
+        return false;
+    }
+
+
+    let n = polygon.length;
+    for (let i = 0; i < n; i++) {
+        let A = polygon[i];
+        let B = polygon[(i + 1) % n]; 
+
+        for (let j = i + 2; j < n - 1; j++) {
+            let C = polygon[j];
+            let D = polygon[(j + 1) % n]; 
+
+            // Skip adjacent segments
+            if (j === (i + 1) % n || i === (j + 1) % n) continue;
+
+            // First, check if bounding boxes intersect
+            if (lineBBIntersection(A, B, C, D)) {
+                // If bounding boxes intersect, check for actual intersection
+
+                if (lineIntersection(A, B, C, D)) {
+                    return true; // Self-intersection found
+                }
+            }
+        }
+    }
+    return false; // No self-intersections
+}
+
+
+
+
+/**
+ * interpolated point 
+ * on segment
+ */
 function pointAtT(pts, t = 0.5) {
 
     /**
@@ -406,6 +680,10 @@ function parsePathDataNormalized(d, options = {}) {
                         case "m":
                         case "l":
                         case "t":
+                            //update last M
+                            if (type === 'm') {
+                                M = { x: values[0] + offX, y: values[1] + offY };
+                            }
                             com.values = [values[0] + offX, values[1] + offY];
                             break;
 
@@ -429,12 +707,24 @@ function parsePathDataNormalized(d, options = {}) {
                                 values[3] + offY
                             ];
                             break;
+
+                        case 'z':
+                        case 'Z':
+                            lastX = M.x;
+                            lastY = M.y;
+                            break;
+
                     }
                 }
                 // is absolute
                 else {
                     offX = 0;
                     offY = 0;
+
+                    // set new M 
+                    if(type==='M'){
+                        M = { x: values[0], y: values[1]};
+                    }
                 }
 
                 /**
